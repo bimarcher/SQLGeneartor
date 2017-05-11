39f79d1ad25c1eb7a6761a9281530d51d99872a0
bimarcher <x_che@126.com>
2017-05-11 23:44:55 +0800

    优化代码结构

--- app-sql.js
+++ app-sql.js
@@ -1,28 +1,11 @@
-const fs = require('fs')
 
 const log = require('./lib/log').Log
 
-const template = require('./lib/template').Template
+const template = require('./lib/template-loader').TemplateLoader
 
 const schemaAnalyzer = require('./lib/schema-analyzer').SchemaAnalyzer
 
 const sqlGenerator = require('./lib/sql-generator').SQLGenerator
 
-const generator = {
-  run: function(schemaConfig){
-    template.init('./template')
-    var sql = sqlGenerator.generateSchemaSql(template.template4Schema, schemaConfig)
-    for (var i = 0; i < schemaConfig.tables.length; i++) {
-      var table = schemaConfig.tables[i]
-      var businessFieldSql = sqlGenerator.generateBusinessFieldSql(template.template4Field, table)
-      sql += sqlGenerator.generateTableSql(template.template4Table, schemaConfig, businessFieldSql, table)
-      sql += sqlGenerator.generateUindexSql(template.template4Uindex, schemaConfig, table)
-    }
-    fs.writeFile('./sql/' + schemaConfig.schemaName + '.sql', sql, function (err) {
-        if (err) throw err
-        log.info('\n\t./sql/' + schemaConfig.schemaName + '.sql is saved!\n\t' + schemaConfig.tables.length + ' tables is created!' )
-    })
-  }
-}
-
-schemaAnalyzer.run('./conf/schema.conf', './conf/schema-config.js', generator);
+template.run('./template')
+schemaAnalyzer.run('./conf/schema.conf', './conf/schema-config.js', template, sqlGenerator);
