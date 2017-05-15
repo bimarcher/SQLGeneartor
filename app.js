af5416060e5744fda6d75b20b35ea889d4c41bac
bimarcher <x_che@126.com>
2017-05-15 21:00:12 +0800

    增加生成sql后直接执行sql创建数据库的命令

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const log = require('./lib/log').Log
-
-log.info('\n\t1.npm run sql : 生成sql语句；\n\t2.npm run db : 执行sql创建数据库')
+const info = '\n\t1.npm run sql : 生成sql语句;' +
+            '\n\t2.npm run db : 执行sql创建数据库;' +
+            '\n\t3.npm run all : 生成sql语句后直接执行sql创建数据库;'
+log.info(info)
