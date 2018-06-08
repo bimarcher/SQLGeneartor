d4b626ccfb42b30cf648ace603de5d00c7aac1ca
bimarcher <x_che@126.com>
2018-06-08 19:04:31 +0800

    优化，使其可以在同一个工程下保存不同的配置文件并基于配置文件执行不同的项目数据库的创建

---
+++ app-run.js
@@ -0,0 +1,179 @@
+const fs = require('fs');
+const exec = require('child_process').exec;
+
+const log = require('./lib/log').Log
+const template = require('./lib/template-loader').TemplateLoader
+const schemaAnalyzer = require('./lib/schema-analyzer').SchemaAnalyzer
+const sqlGenerator = require('./lib/sql-generator').SQLGenerator
+const mysqlDBList = require('./conf/db-config').dbList;
+
+const Runner = {
+  run: function(scripts, distPath, schemaName) {
+    if (scripts.length===1){
+      this.doRun(scripts[0], distPath, schemaName);
+      log.info(`^-@~@-^【${scripts[0]}】执行完成！！！！！！\n`);
+      return;
+    }
+    if (scripts.length===2){
+      this.doRun(scripts[0], distPath, schemaName).then((tag)=>{
+        log.info(`^-@~@-^【${scripts[0]}】执行完成！！！！！！\n`);
+        return this.doRun(scripts[1], distPath, schemaName);
+      }).then((tag)=>{
+        log.info(`^-@~@-^【${scripts[1]}】执行完成！！！！！！\n`);
+      }).catch((tag)=>{
+        log.info(`^-@~@-^【${tag}】执行出错！！！！！！\n`);
+      });
+      return;
+    }
+    if (scripts.length===3){
+      this.doRun(scripts[0], distPath, schemaName).then((tag)=>{
+        log.info(`^-@~@-^【${scripts[0]}】执行完成！！！！！！\n`);
+        return this.doRun(scripts[1], distPath, schemaName);
+      }).then((tag)=>{
+        log.info(`^-@~@-^【${scripts[1]}】执行完成！！！！！！\n`);
+        return this.doRun(scripts[2], distPath, schemaName);
+      }).then((tag)=>{
+        log.info(`^-@~@-^【${scripts[2]}】执行完成！！！！！！\n`);
+      }).catch((tag)=>{
+        log.info(`^-@~@-^【${tag}】执行出错！！！！！！\n`);
+      });
+      return;
+    }
+    log.info(`\n\t输入的命令${scripts.join(",")}无效!\n`);
+  },
+  doRun: function(type, distPath, schemaName) {
+    return new Promise((resolve, reject) => {
+      if (type === "clean"){
+        this.clean(distPath).then((tag)=>{
+          if(tag==='success'){
+            resolve('success');
+          }else{
+            reject('run:doRun:clean:failure');
+          }
+        });
+        return;
+      }
+
+      if (type === "sql"){
+        this.schemaAnalyzer(distPath, schemaName).then((tag)=>{
+          if(tag==='success'){
+            resolve('success');
+          }else{
+            reject('run:doRun:sql:failure');
+          }
+        });
+        return;
+      }
+
+      if (type === "db"){
+        this.dbCreator(distPath, schemaName).then((tag)=>{
+          if(tag==='success'){
+            resolve('success');
+          }else{
+            reject('run:doRun:db:failure');
+          }
+        });
+        return;
+      }
+      log.info(`\n\t输入的命令${type}无效!\n`);
+      reject('run:doRun:failure');
+    });
+  },
+  checkArgv: function(distPath, schemaName, sqlFile) {
+    if (!fs.existsSync(distPath)){
+      log.info(`\n\t不存在路径为【${distPath}】的输出目录！！！！！！\n`);
+      return false;
+    }
+    if (!schemaName){
+      log.info('\n\t请在命令中指定schema！！！！！！\n');
+      return false;
+    }
+    const schemaPath = `./conf/${schemaName}.conf`;
+    if (!fs.existsSync(schemaPath)){
+      log.info(`\n\t不存在路径为【${schemaPath}】的schema的配置文件！！！！！！\n`);
+      return false;
+    }
+    if (sqlFile && !fs.existsSync(sqlFile)){
+      log.info(`\n\t不存在路径为【${sqlFile}】的sql文件！！！！！！\n`);
+      return false;
+    }
+    return true;
+  },
+  clean: function(distPath) {
+    var deleteFolder = function(path) {
+      var files = [];
+      if( fs.existsSync(path) ) {
+          files = fs.readdirSync(path);
+          files.forEach(function(file,index){
+              var curPath = path + "/" + file;
+              if(fs.statSync(curPath).isDirectory()) { // recurse
+                  deleteFolder(curPath);
+              } else { // delete file
+                  fs.unlinkSync(curPath);
+              }
+          });
+          fs.rmdirSync(path);
+      }
+    };
+    return new Promise((resolve, reject) => {
+      deleteFolder(distPath);
+      log.info(`\n\t清除输出文件夹${distPath}成功!\n`);
+      
+      fs.mkdirSync(distPath);
+      log.info(`\n\t创建输出文件夹${distPath}成功!\n`);
+
+      resolve('success');
+    });
+  },
+  schemaAnalyzer: function(distPath, schemaName) {
+    return new Promise((resolve, reject) => {
+      if (!distPath.endsWith("/")){
+        distPath += "/";
+      }
+      if (!this.checkArgv(distPath,schemaName)){
+        return;
+      }
+      template.run('./template');
+      schemaAnalyzer.run(`./conf/${schemaName}.conf`, `${distPath}${schemaName}.config.js`,
+      `${distPath}${schemaName}.sql`, template, sqlGenerator).then((tag)=>{
+        if(tag==='success'){
+          resolve('success');
+        }else{
+          reject('run:schemaAnalyzer:failure');
+        }
+      });
+    });
+  },
+  dbCreator: function(distPath, schemaName){
+    return new Promise((resolve, reject) => {
+      if (!distPath.endsWith("/")){
+        distPath += "/";
+      }
+      const sqlFile = `${distPath}${schemaName}.sql`;
+      if (!this.checkArgv(distPath,schemaName, sqlFile)){
+        reject('run:dbCreator:failure');
+        return;
+      }
+      for(let i = 0; i < mysqlDBList.length; i++){
+        const mysqlDB = mysqlDBList[i]
+        const script = 'mysql -h ' + mysqlDB.host + ' -u' + mysqlDB.user + ' -p' + mysqlDB.pw + ' < ' + sqlFile;
+        exec(script, (error, stdout, stderr) => {
+          if (error) {
+            log.info(`\n\texec error: ${error}`)
+            reject('run:dbCreator:failure');
+            return
+          }
+          // log.info(`\n\tstdout: ${stdout}`);
+          // log.info(`\n\tstderr: ${stderr}`);
+          log.info('\n\t' + mysqlDB.host + ' is updated!\n')
+          resolve('success');
+        })
+      }
+    });
+  }
+}
+
+const scripts = process.argv[2].split(",");
+const distPath = process.argv[3];
+const schemaName = process.argv[4];
+Runner.run(scripts, distPath, schemaName);
--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const log = require('./lib/log').Log
-const info = '\n\t1.npm run sql : 生成sql语句;' +
-            '\n\t2.npm run db : 执行sql创建数据库;' +
-            '\n\t3.npm run all : 生成sql语句后直接执行sql创建数据库;'
+const info = 
+  '\n\t0.npm run clean : ' +
+  '\n\t\t清理输出目录的所有文件;\n' +
+  '\n\t1.npm run sql [schemaConfigName] : ' +
+  '\n\t\t只生成[schemaConfigName]对应的sql脚本;\n' +
+  '\n\t2.npm run db [schemaConfigName] : ' +
+  '\n\t\t只基于[schemaConfigName]对应的sql脚本创建数据库;\n' +
+  '\n\t3.npm run clean,sql [schemaConfigName] :' +
+  '\n\t\t清理输入目录后，生成[schemaConfigName]对应的sql脚本;\n' +
+  '\n\t4.npm run sql,db [schemaConfigName] : ' +
+  '\n\t\t生成[schemaConfigName]对应的sql脚本并创建数据库;\n' +
+  '\n\t5.npm run clean,sql,db [schemaConfigName]: ' +
+  '\n\t\t清理输入目录后，生成[schemaConfigName]对应的sql脚本本并创建数据库;\n'
+  '\n\t6.npm run all [schemaConfigName]: ' +
+  '\n\t\t清理输入目录后，生成[schemaConfigName]对应的sql脚本本并创建数据库;\n'
 log.info(info)
