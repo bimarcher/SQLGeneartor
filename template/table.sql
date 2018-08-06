9be82cd8c0a288e2e15a159dc522d1480144941f
bimarcher <x_che@126.com>
2018-08-06 14:30:13 +0800

    1.timestamp设置精度为3位;2.sign的长度设置为100

--- template/table.sql
+++ template/table.sql
@@ -6,9 +6,9 @@ CREATE TABLE {{schemaName}}.{{tableName}}
 {{businessFileSql}}
   r_memo VARCHAR(100) NULL COMMENT '本条记录的备注',
   r_status INT(1) DEFAULT '1' NOT NULL COMMENT '本条记录的状态 0 无效 1 有效（默认值）其他待定义',
-  r_create_at TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '本条记录的创建时间',
+  r_create_at TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP(3) NOT NULL COMMENT '本条记录的创建时间',
   r_create_by VARCHAR(25) DEFAULT '{{schemaName}}' NOT NULL COMMENT '本条记录的创建人',
-  r_modify_at TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '本条记录的最后一次修改时间',
+  r_modify_at TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP(3) NOT NULL COMMENT '本条记录的最后一次修改时间',
   r_modify_by VARCHAR(25) DEFAULT '{{schemaName}}' NOT NULL COMMENT '本条记录的最后一次修改人',
   PRIMARY KEY (`{{primaryKey}}`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '{{tableComment}}';
